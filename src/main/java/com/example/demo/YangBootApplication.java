19bd045017912a1997de884bf3ad18100ced2d76
sanghwan <jae6120@naver.com>
2019-02-08 01:27:18 +0900

    logging springboot beans

--- src/main/java/com/example/demo/YangBootApplication.java
+++ src/main/java/com/example/demo/YangBootApplication.java
@@ -1,13 +1,24 @@
 package com.example.demo;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 public class YangBootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(YangBootApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(YangBootApplication.class, args);
+	    String[] beanNames = ctx.getBeanDefinitionNames();
+	    Arrays.sort(beanNames);
+	    for (String beanName : beanNames) {
+	    	log.info("beans : "+beanName);
+	    }
 	}
 
 }
