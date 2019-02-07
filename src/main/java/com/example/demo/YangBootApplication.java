421e98c3406e898aba0cc6fae4c9b71fd0841a3b
sanghwan <jae6120@naver.com>
2019-02-08 01:15:22 +0900

    spring-boot-starter

---
+++ src/main/java/com/example/demo/YangBootApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YangBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YangBootApplication.class, args);
+	}
+
+}
+
